164ae671ac224092655427de77b6d5707764a156
Rag88 <4555@holbertonschool.com>
2022-04-08 23:57:06 +0000

    Main

--- main.h
+++ main.h
@@ -2,25 +2,11 @@
 #define MAIN_H
 
 #include <stdarg.h> /* va_list */
-#include <stdlib.h> /* malloc, free */
 #include <unistd.h> /* write */
-
-/**
- * struct formate - struct to choose the format
- * @ftype: type of format to print
- * @funty: function for printing
- */
-
-typedef struct formate
-{
-	char *ftype;
-	char* (*funty)(va_list);
-} f_type;
 
 /* printf functions */
 int _putchar(char c);
 int _printf(const char *format, ...);
 int _putstring(char *s);
-int _putint(int num);
 
 #endif
